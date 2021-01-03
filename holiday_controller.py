8b71156fa1767a332c44f01055cc4f9e828a346b
Lee Raulin <leeraulin@gmail.com>
2021-01-03 13:55:51 -0500

    ...

--- holiday_controller.py
+++ holiday_controller.py
@@ -7,19 +7,24 @@ HOLIDAY_MULTIPLIER = 0.5
 # Column Headers
 CREATED_AT = 'Created At (UTC)'
 HOURS_WORKED = 'Hours Worked'
-HOLIDAY_HOURS = 'X-Holiday Hours'
-HOLIDAY_PAY = 'X-Holiday Pay'
+HOLIDAY_HOURS = 'HOL'
+HOLIDAY_PAY = 'Adjustment'
 LUNCH = 'Lunch (in mins)'
-NONHOLIDAY_HOURS = 'X-Non-holiday Hours'
-NONHOLIDAY_PAY = 'X-Non-Holiday Pay'
-OVERTIME_HOURS_WORKED = 'Overtime Hours Worked'
-OVERTIME_PAY_RATE = 'Overtime Pay Rate'
-PAY_RATE = 'Pay Rate'
-REGULAR_HOURS_WORKED = 'Regular Hours Worked'
-TOTAL_PAY = 'X-Total Pay'
+NON_HOLIDAY_HOURS = 'X-Non-holiday Hours'
+NON_HOLIDAY_PAY = 'X-Non-Holiday Pay'
+TOTAL_PAY = 'Total Pay'
 START_TIME = 'Start Time'
 END_TIME = 'End Time'
 NAME = 'Name'
+## Pay Rates
+PAY_RATE = 'Pay Rate'
+OVERTIME_PAY_RATE = 'Overtime Pay Rate'
+DOUBLETIME_RATE = 'Doubletime Pay Rate'
+STIPEND = 'Stipend (Pro-rated)'
+## Hours
+REGULAR_HOURS_WORKED = 'Regular Hours Worked'
+OVERTIME_HOURS_WORKED = 'Overtime Hours Worked'
+DOUBLETIME_HOURS_WORKED = 'Doubletime Hours Worked'
 
 
 def delta_to_hours(delta: timedelta):
@@ -39,13 +44,13 @@ def calc_holiday_hours(holiday: datetime, row) -> float:
     elif start_time.date() == holiday.date() and end_time.date() > holiday.date():
         holiday_hours = delta_to_hours(holiday_end - start_time)
         holiday_hours = round(holiday_hours, 2)
-        return min(row[REGULAR_HOURS_WORKED],  holiday_hours)
+        return min(row[REGULAR_HOURS_WORKED], holiday_hours)
     # shift starts before, ends on holiday - >
     elif start_time.date() < holiday.date() and end_time.date() == holiday.date():
         overtime = row[OVERTIME_HOURS_WORKED]
         holiday_hours = delta_to_hours(end_time - holiday)
         holiday_hours = round(holiday_hours, 2)
-        return max(holiday_hours - overtime,  0)
+        return max(holiday_hours - overtime, 0)
     # shift not on holiday -> 0
     else:
         return 0
@@ -55,18 +60,36 @@ def calc_holiday_pay(row) -> float:
     return round(row[PAY_RATE] * row[HOLIDAY_HOURS] * HOLIDAY_MULTIPLIER, 2)
 
 
+def calc_total_pay(row):
+    return round(row[PAY_RATE] * row[REGULAR_HOURS_WORKED] + row[OVERTIME_PAY_RATE] * row[OVERTIME_HOURS_WORKED] + row[
+        DOUBLETIME_RATE] * row[DOUBLETIME_HOURS_WORKED] + row[STIPEND] + row[HOLIDAY_PAY], 2)
+
+
 def process_csv(holiday: datetime, csv_data: str) -> str:
-    cols_to_keep = [CREATED_AT, NAME, START_TIME, END_TIME, LUNCH, HOURS_WORKED, REGULAR_HOURS_WORKED, OVERTIME_HOURS_WORKED, PAY_RATE, OVERTIME_PAY_RATE]
+    cols_to_keep = [CREATED_AT, NAME, START_TIME, END_TIME, LUNCH, HOURS_WORKED, REGULAR_HOURS_WORKED,
+                    OVERTIME_HOURS_WORKED, PAY_RATE, OVERTIME_PAY_RATE, DOUBLETIME_HOURS_WORKED, DOUBLETIME_RATE,
+                    STIPEND]
     df = pd.read_csv(StringIO(csv_data)).filter(cols_to_keep, axis=1)
 
+    # Fill missing number cells with 0
     df[LUNCH] = df[LUNCH].fillna(0)
+    df[PAY_RATE] = df[PAY_RATE].fillna(0)
+    df[REGULAR_HOURS_WORKED] = df[REGULAR_HOURS_WORKED].fillna(0)
+    df[OVERTIME_PAY_RATE] = df[OVERTIME_PAY_RATE].fillna(0)
+    df[OVERTIME_HOURS_WORKED] = df[OVERTIME_HOURS_WORKED].fillna(0)
+    df[DOUBLETIME_RATE] = df[DOUBLETIME_RATE].fillna(0)
+    df[DOUBLETIME_HOURS_WORKED] = df[DOUBLETIME_HOURS_WORKED].fillna(0)
+
+    # Add calculated columns
     df[HOLIDAY_HOURS] = df.apply(lambda row: calc_holiday_hours(holiday, row), axis=1)
     df[HOLIDAY_PAY] = df.apply(lambda row: calc_holiday_pay(row), axis=1)
+    df[TOTAL_PAY] = df.apply(lambda row: calc_total_pay(row), axis=1)
 
+    df = df[[CREATED_AT, NAME, START_TIME, END_TIME, HOURS_WORKED, OVERTIME_HOURS_WORKED, PAY_RATE, HOLIDAY_HOURS, HOLIDAY_PAY, TOTAL_PAY]]
     df.sort_values(CREATED_AT)
-    df = df.drop(LUNCH, 1)
-    df = df.drop(REGULAR_HOURS_WORKED, 1)
-    df = df.drop(OVERTIME_PAY_RATE, 1)
 
-    return df.to_csv()
+    # Find entries needing admin approval
+    needs_super_admin = df[TOTAL_PAY] >= 2000
+    names_needing_approval = list(set(df[needs_super_admin][NAME]))
 
+    return {'csv': df.to_csv(), 'super_admin_list': names_needing_approval}
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import holiday_controller as hc
 app: Flask = Flask(__name__)
 CORS(app)
 
+
 @app.route('/holiday', methods=['POST'])
 def calculate_pay():
     body = request.json
-    print(body)
     # Get date from query parameter
     holiday: datetime = datetime.strptime(body['date'], "%Y-%m-%d")
     # Get CSV from request body
@@ -19,20 +19,6 @@ def calculate_pay():
     output = hc.process_csv(holiday, csv)
 
     return jsonify(output)
-
-
-def _build_cors_prelight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
-
-
-def _corsify_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    return response
 
 
 if __name__ == '__main__':
