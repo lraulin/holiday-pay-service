4558726bb741605ae5103ee558e0f4c32fea8898
Lee Raulin <leeraulin@gmail.com>
2021-01-21 14:05:19 -0500

    ...

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, make_response
 from flask_cors import cross_origin
 from datetime import datetime
+from dateparser import parse
 import holiday_controller as hc
 
 app: Flask = Flask(__name__)
@@ -10,15 +11,14 @@ app: Flask = Flask(__name__)
 @cross_origin()
 def calculate_pay():
     body = request.json
-    # Get date from query parameter
-    holiday: datetime = datetime.strptime(body['date'], "%Y-%m-%d")
+    # Get date from request body
+    holiday: datetime = parse(body['date'])
     # Get CSV from request body
     csv: str = body['csv']
 
     # Process data
     output = hc.process_csv(holiday, csv)
     response = jsonify(output)
-    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 
